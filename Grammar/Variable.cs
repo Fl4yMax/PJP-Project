896682f7956aeee1d641895bbff0103fbaa97215
Fl4yMax <gabrielpaz703@gmail.com>
2024-04-22 17:49:21 +0200

    Structure for storing type and value of a declared variable

--- Grammar/Variable.cs
+++ Grammar/Variable.cs
@@ -8,6 +8,12 @@ namespace Lab3
 {
     internal class Variable
     {
+        public Variable(MyType type, object value)
+        {
+            this.type = type;
+            this.value = value;
+        }
+
         public MyType type { get; set; }
         public Object value { get; set; }
     }
